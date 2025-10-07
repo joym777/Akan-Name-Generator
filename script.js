e31868f51ae15ff9136d87d860646542187e1278
joym777 <joymuteti22@gmail.com>
2025-10-07 10:37:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Array of akan names
+//Array of akan names
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
 //Stop page refresh while form submits
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -11,7 +10,6 @@ document.querySelector("form").addEventListener("submit", function(event) {
     let month = parseInt(document.querySelector("#month").value);
     let year = parseInt(document.querySelector("#year").value);
     let gender = document.querySelector("#user-gender").value;
-
     // adjusted months (Jan, Feb as 13, 14 of previous year)
     if(month < 3) {
         month += 12;
@@ -22,13 +20,11 @@ document.querySelector("form").addEventListener("submit", function(event) {
     let yy = year % 100;
     let mm = month;
     let dd = day;
-
     //Formula for the day of the week
     let d = ((cc / 4 - 2 * cc - 1) + ((5 * yy) / 4) + ((26 * (mm + 1)) / 10) + dd) % 7;
-
     //Ensure positive and day to range 0–6
     let dayOfWeek = Math.floor((d + 7) % 7);
-
+    
     //Akan name selection
     let akanName = "";
     if (gender === "Male") {
